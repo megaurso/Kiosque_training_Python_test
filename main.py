6cf27a026aaaddc82d70926425dec092d852e965
megaurso <bernadogui@hotmail.com>
2023-03-23 15:07:51 -0300

    feat: finish taks 3 and all done

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from management.product_handler import get_product_by_id,get_products_by_type,add_product
+from management.product_handler import (
+    get_product_by_id,
+    get_products_by_type,
+    add_product,
+    menu_report,
+    add_product_extra,
+)
 from menu import products
 from management.tab_handler import calculate_tab
 
@@ -11,9 +17,9 @@ if __name__ == "__main__":
         "price": 5.0,
         "rating": 5,
         "description": "Sanduiche de Python",
-        "type": "fast-food"
+        "type": "fast-food",
     }
-    print(add_product(products,**new_product))
+    print(add_product(products, **new_product))
     table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
     table_2 = [
         {"_id": 10, "amount": 3},
@@ -22,4 +28,16 @@ if __name__ == "__main__":
     ]
     print(calculate_tab(table_1))
     print(calculate_tab(table_2))
-    
+    print(menu_report())
+
+    required_keys = ("description", "price", "rating", "title", "type")
+    new_product = {
+        "title": "X-Python",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Python",
+        "type": "fast-food",
+        "extra_key_1": "extra_value_1",
+        "extra_key_2": "extra_value_2",
+    }
+    print(add_product_extra(products, *required_keys, **new_product))
--- management/product_handler.py
+++ management/product_handler.py
@@ -1,13 +1,21 @@
 from menu import products
+from collections import Counter
 
-def get_product_by_id(id):
+
+def get_product_by_id(id: int):
+    if not isinstance(id, int):
+        raise TypeError("product id must be an int")
+
     for product in products:
         if product["_id"] == id:
             return product
     return {}
 
 
-def get_products_by_type(type):
+def get_products_by_type(type: str):
+    if not isinstance(type, str):
+        raise TypeError("product type must be a str")
+
     result = []
     for product in products:
         if product["type"] == type:
@@ -15,7 +23,7 @@ def get_products_by_type(type):
     return result
 
 
-def add_product(menu,**new_product):
+def add_product(menu, **new_product):
     id = 1
     if not menu:
         new_product["_id"] = id
@@ -24,13 +32,52 @@ def add_product(menu,**new_product):
         if item_id["_id"] >= id:
             id = item_id["_id"] + 1
 
-
-      
     new_product["_id"] = id
 
     menu.append(new_product)
-    
-    print(new_product)
 
     return new_product
-    
+
+
+def menu_report():
+    product_count = 0
+    price = 0
+
+    for index, value in enumerate(products):
+        product_count = index + 1
+
+    for product in products:
+        price += product["price"]
+
+    average_price = price / product_count
+
+    counter = Counter(product["type"] for product in products)
+
+    most_common_type = counter.most_common(1)[0][0]
+
+    return f"Products Count: {product_count} - Average Price: ${round(average_price,2)} - Most Common Type: {most_common_type}"
+
+
+def add_product_extra(menu, *required_keys, **new_product):
+    id = 1
+    for key in required_keys:
+        if key not in new_product:
+            raise KeyError(f"field {key} is required")
+
+    extra_key = set(new_product.keys()).symmetric_difference(set(required_keys))
+
+    for key in extra_key:
+        new_product.pop(key)
+
+    if not menu:
+        new_product["_id"] = id
+
+    for item_id in menu:
+        if item_id["_id"] >= id:
+            id = item_id["_id"] + 1
+
+    new_product["_id"] = id
+
+    menu.append(new_product)
+
+    return new_product
